d9bcb99b36453634f31534ae7d102bf93c163bf5
asp <asp@aptechnn2.edu>
2025-01-02 11:00:47 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Button } from "flowbite-react";
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
