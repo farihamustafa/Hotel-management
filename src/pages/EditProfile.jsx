81adb4b902ae4d3c84f692e4a7243877977761c5
arsshhkhan <zarshkhan4218@gmail.com>
2025-01-17 19:31:23 +0500

    Update EditProfile.jsx

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -40,7 +40,7 @@ function EditProfile() {
           <FiX className="h-6 w-6" aria-hidden="true" />
         </button>
 
-        <h1 className="text-4xl font-bold text-gray-800 mb-6 text-left border-b pb-4 border-gray-300">
+        <h1 className="text-4xl font-bold text-gray-800 mb-6  border-b pb-4 text-center border-gray-300">
           Edit Profile
         </h1>
 
